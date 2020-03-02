87d1affaf3c48becd3eed0a3ff6c3a074439d361
Tianyi Liu <liuty10@gmail.com>
2020-03-02 12:03:36 -0600

    need to deal with multiple tid

--- interceptgl/util.c
+++ interceptgl/util.c
@@ -1,11 +1,12 @@
 #include "util.h"
-#define NUM_THREADS 6
 
+#define NUM_THREADS 6 //should larger or equal to 1
 #define NUM_COUNTERS 4
+
 static int       perfstart_flag = 0; 
 static uint64_t  count[NUM_COUNTERS];
 
-thread_pfm_context_t thread_ctx;
+thread_pfm_context_t thread_ctx[NUM_THREADS];
 
 /*
  * Initilization
@@ -70,49 +71,49 @@ void perf_start(char * evns){
     	if(perfstart_flag == 0){
 		/* initialize PMU monitoring */
        		int ret = pfm_operations_init();
-       		if(ret != 0 )
-       		        errx(1, "PMU initialization failed\n");
+       		if(ret != 0 ) errx(1, "PMU initialization failed\n");
+		for(int idx=0; idx< NUM_THREADS; idx++){
+			thread_ctx[idx].tid = 0;
+        		thread_ctx[idx].fds = NULL;
+        		thread_ctx[idx].num_fds = 0;
 
-		thread_ctx.tid = 0;
-        	thread_ctx.fds = NULL;
-        	thread_ctx.num_fds = 0;
+			ret = perf_setup_list_events(evns, &(thread_ctx[idx].fds),
+			                                   &(thread_ctx[idx].num_fds));
+			if(ret || !(thread_ctx[idx].num_fds))
+				return;
 
-	        ret = perf_setup_list_events(evns, &(thread_ctx.fds),
-	                               &(thread_ctx.num_fds));
-	        if(ret || !(thread_ctx.num_fds))
-	        	return;
-
-		for(int i = 0; i < thread_ctx.num_fds; i++){
-		   thread_ctx.fds[i].hw.disabled = 1;
-		   thread_ctx.fds[i].hw.exclude_user = 0;
-		   thread_ctx.fds[i].hw.exclude_kernel = 0;
-		   thread_ctx.fds[i].hw.exclude_hv = 1;
-		   thread_ctx.fds[i].hw.inherit = 0; /* only monitor the current thread */
-		
-		   thread_ctx.fds[i].fd = perf_event_open(&(thread_ctx.fds[i].hw), 
-							    thread_ctx.tid, -1, -1, 0);
-		   if (thread_ctx.fds[i].fd == -1) {
-		           warn("cannot attach event%d %s to thread [%d]: ", i,
-		                thread_ctx.fds[i].name, thread_ctx.tid);
-		           return;
-		   }
-		   DPRINTF("PMU context opened for thread [%d]\n", thread_ctx.tid);
+			for(int j = 0; j < thread_ctx[idx].num_fds; j++){
+			   thread_ctx[idx].fds[j].hw.disabled = 1;
+			   thread_ctx[idx].fds[j].hw.exclude_user = 0;
+			   thread_ctx[idx].fds[j].hw.exclude_kernel = 0;
+			   thread_ctx[idx].fds[j].hw.exclude_hv = 1;
+			   thread_ctx[idx].fds[j].hw.inherit = 0; /* only monitor the current thread */
+			   thread_ctx[idx].fds[j].fd = perf_event_open(&(thread_ctx[idx].fds[j].hw), 
+								         thread_ctx[idx].tid, -1, -1, 0);
+			   if (thread_ctx[idx].fds[j].fd == -1) {
+			           warn("cannot attach event%d %s to thread [%d]: ", j,
+			                thread_ctx[idx].fds[j].name, thread_ctx[idx].tid);
+			           return;
+			   }
+			   DPRINTF("PMU context opened for thread [%d]\n", thread_ctx[idx].tid);
+			}
 		}
         }
         perfstart_flag += 1;
-        if(perfstart_flag == 7) perfstart_flag = 1;
-	for(int i = 0; i < thread_ctx.num_fds; i++){
-		ioctl(thread_ctx.fds[i].fd, PERF_EVENT_IOC_RESET, 0);
-		ioctl(thread_ctx.fds[i].fd, PERF_EVENT_IOC_ENABLE, 0);
+        if(perfstart_flag == NUM_THREADS+1) perfstart_flag = 1;
+
+	for(int i = 0; i < thread_ctx[perfstart_flag-1].num_fds; i++){
+		ioctl(thread_ctx[perfstart_flag-1].fds[i].fd, PERF_EVENT_IOC_RESET, 0);
+		ioctl(thread_ctx[perfstart_flag-1].fds[i].fd, PERF_EVENT_IOC_ENABLE, 0);
 	}
 }
 
 void perf_end(){
     	if(perfstart_flag != 0){
-		for(int i = 0; i < thread_ctx.num_fds; i++){
-			ioctl(thread_ctx.fds[i].fd, PERF_EVENT_IOC_DISABLE, 0);
-			read(thread_ctx.fds[i].fd, &count[i], sizeof(uint64_t));
-			MY_LOG("%ld\n",count[i]);
+		for(int i = 0; i < thread_ctx[perfstart_flag-1].num_fds; i++){
+			ioctl(thread_ctx[perfstart_flag-1].fds[i].fd, PERF_EVENT_IOC_DISABLE, 0);
+			read (thread_ctx[perfstart_flag-1].fds[i].fd, &count[i], sizeof(uint64_t));
+			MY_LOG("thread%d : event%d : %ld\n",perfstart_flag-1,i,count[i]);
 		}
         }else{
 		MY_LOG(" Perf didn't start, START FIRST\n");
