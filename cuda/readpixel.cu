3f6ed650bf633d8166e8bcc563dc3bd5068e5ac9
Tianyi Liu <liuty10@gmail.com>
2020-05-20 17:29:18 -0500

    yuv 444

--- cuda/readpixel.cu
+++ cuda/readpixel.cu
@@ -1,4 +1,5 @@
 // g++ readpixel.cpp -o readpixel -lGL -lglut
+
 #define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
 #include <GL/glext.h>
@@ -16,8 +17,30 @@ struct cudaGraphicsResource *cuda_pbo_dest_resource;
 // Some dummy kernel to prevent optimizations
 __global__ void kernel(char* in)
 {
-    int index = threadIdx.x + blockIdx.x*blockDim.x;
-    in[index] = in[index]-1;
+    //int index = threadIdx.x + blockIdx.x*blockDim.x;
+    //in[index] = in[index]-1;
+}
+
+//__global__ void rgb2yuv444Kernel(unsigned char* rgbImg, unsigned char* yuvOut){
+__global__ void rgb2yuv444Kernel(unsigned char* rgbImg){
+ 
+    unsigned char r, g, b;
+    unsigned char y, cb, cr;
+ 
+    int index;
+    index = (threadIdx.x + blockIdx.x * blockDim.x)*3;
+    
+    r = rgbImg[index + 0];
+    g = rgbImg[index + 1];
+    b = rgbImg[index + 2];
+     
+    y  = (unsigned char)( 0.299*r + 0.587*g +  0.114*b);
+    cb = (unsigned char)(-0.169*r - 0.331*g +  0.499*b + 128);
+    cr = (unsigned char)( 0.499*r - 0.418*g - 0.0813*b + 128);
+     
+    rgbImg[index + 0] = y;
+    rgbImg[index + 1] = cb;
+    rgbImg[index + 2] = cr;
 }
 
 #define cutilSafeCall(err)  __cudaSafeCall(err,__FILE__,__LINE__)
@@ -54,24 +77,22 @@ void display()
 
     // 4 bytes per pixel (RGBA), 1x1 bitmap
     if(!usepbo){
-        unsigned char pixels[ 1 * 1 * 4 ] = { 0 };
-        glReadPixels( mx, my, 1, 1, GL_RGBA, GL_UNSIGNED_BYTE, pixels );
+        unsigned char pixels[ 1 * 1 * 3 ] = { 0 };
+        glReadPixels( mx, my, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, pixels );
         printf("r: %d\n", pixels[ 0 ] );
         printf("g: %d\n", pixels[ 1 ] );
         printf("b: %d\n", pixels[ 2 ] );
-        printf("a: %d\n", pixels[ 3 ] );
     }else{
         glReadBuffer(GL_FRONT);
         glBindBuffer(GL_PIXEL_PACK_BUFFER, pbo);
-        glReadPixels(0, 0, WIDTH, HEIGHT, GL_BGRA, GL_UNSIGNED_BYTE, 0);
-        //unsigned char* pboBits = NULL;
-        //pboBits = (unsigned char *)glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
-        //if(!pboBits){printf("Could not map pboBits\n"); return;}
-        //printf("r: %d\n", pboBits[(my*WIDTH+ mx)*4 + 0 ] );
-        //printf("g: %d\n", pboBits[(my*WIDTH+ mx)*4 + 1 ] );
-        //printf("b: %d\n", pboBits[(my*WIDTH+ mx)*4 + 2 ] );
-        //printf("a: %d\n", pboBits[(my*WIDTH+ mx)*4 + 3 ] );
-        //glUnmapBuffer(GL_PIXEL_PACK_BUFFER_EXT);
+        glReadPixels(0, 0, WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, 0);
+        unsigned char* pboBits = NULL;
+        pboBits = (unsigned char *)glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
+        if(!pboBits){printf("Could not map pboBits\n"); return;}
+        printf("r: %d\n", pboBits[(my*WIDTH+ mx)*3 + 0 ] );
+        printf("g: %d\n", pboBits[(my*WIDTH+ mx)*3 + 1 ] );
+        printf("b: %d\n", pboBits[(my*WIDTH+ mx)*3 + 2 ] );
+        glUnmapBuffer(GL_PIXEL_PACK_BUFFER_EXT);
 
         cutilSafeCall(cudaGraphicsMapResources(1, &cuda_pbo_dest_resource));
         size_t size = 0;
@@ -79,9 +100,24 @@ void display()
         cutilSafeCall( cudaGraphicsResourceGetMappedPointer(&device_ptr, &size, cuda_pbo_dest_resource) );
         glBindBuffer(GL_PIXEL_PACK_BUFFER_EXT, 0);
 
-        kernel<<<size/512,512>>>((char*)device_ptr);
+        //kernel<<<size/512,512>>>((char*)device_ptr);
+        unsigned char* yuvOut;
+        cudaMalloc((void**)&yuvOut, size);
+        rgb2yuv444Kernel<<<WIDTH*HEIGHT/256,256>>>((unsigned char*)device_ptr);
+        //rgb2yuv444Kernel<<<WIDTH*HEIGHT/256,256>>>((unsigned char*)device_ptr,yuvOut);
         cudaDeviceSynchronize();
         cutilSafeCall( cudaGraphicsUnmapResources(1, &cuda_pbo_dest_resource) );
+
+        
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, pbo);
+        pboBits = NULL;
+        pboBits = (unsigned char *)glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
+        if(!pboBits){printf("Could not map pboBits\n"); return;}
+        printf("y: %d\n", pboBits[(my*WIDTH+ mx)*3 + 0 ] );
+        printf("Cb: %d\n", pboBits[(my*WIDTH+ mx)*3 + 1 ] );
+        printf("Cr: %d\n", pboBits[(my*WIDTH+ mx)*3 + 2 ] );
+        glUnmapBuffer(GL_PIXEL_PACK_BUFFER_EXT);
+        glBindBuffer(GL_PIXEL_PACK_BUFFER_EXT, 0);
 
     }
 
